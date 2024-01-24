d007a736bd5f3249de50fbda05d9bbb7a4ede6c0
tsmame767 <tsmane789@gmail.com>
2024-01-24 16:31:19 +0530

    prog3

---
+++ prog3/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog3
+{
+    internal class main
+    {
+
+        static void Main()
+        {
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+            double[] arr2 = { 2.6543, 4.7654, 6.876543, 4.98, 34.876 };
+            string[] arr3 = { "Tushar", "Hemant", "Vikas" };
+            DeleteArray<int> obj = new DeleteArray<int>();
+            DeleteArray<double> obj1 = new DeleteArray<double>();
+            DeleteArray<string> obj2 = new DeleteArray<string>();
+
+            obj1.DeleteMethod(arr2, 2.6543);
+            obj2.DeleteMethod(arr3, "Tushar");
+            obj.DeleteMethod(arr, 5);
+        }
+    }
+}
---
+++ prog3/Class2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog3
+{
+    public class DeleteArray<T> where T : IComparable<T>
+    {
+        public void DeleteMethod(T[] arr, T element)
+        {
+            var newarr = Array.Empty<T>();
+
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i].CompareTo(element) == 0)
+                    {
+                        continue;
+                    }
+                   
+                
+
+                else if (arr[i].CompareTo(element) == 0)
+                    {
+                        continue;
+                    }
+                    
+                else if (arr[i].CompareTo(element) == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    newarr = newarr.Append(arr[i]).ToArray();
+                    Console.Write(arr[i] + ", ");
+                }
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                Console.Write(newarr[i] + ", ");
+            }
+            Console.WriteLine(element.GetType().Name);
+        }
+
+
+    }
+}
