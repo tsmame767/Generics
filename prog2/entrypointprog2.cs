8996969c7c2fc44086c3ea08c71627677a0ed31f
tsmame767 <tsmane789@gmail.com>
2024-01-24 15:44:47 +0530

    prog2

---
+++ prog2/entrypointprog2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2
+{
+    internal class entrypointprog2
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            double[] arr2 = { 2.6543, 4.7654, 6.876543, 4.98, 34.876 };
+            string[] arr3 = { "Tushar", "Hemant", "Vikas" };
+            DeleteArray<int> obj = new DeleteArray<int>();
+            DeleteArray<double> obj1 = new DeleteArray<double>();
+            DeleteArray<string> obj2 = new DeleteArray<string>();
+
+            obj1.DeleteMethod(arr2, 2.6543);
+            obj2.DeleteMethod(arr3, "Tushar");
+            obj.DeleteMethod(arr, 5);
+
+        }
+    }
+}
---
+++ prog2/program2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2
+{
+    public class DeleteArray<T> where T : IComparable<T>
+    {
+        public void DeleteMethod(T[] arr, T element)
+        {
+            var newarr = Array.Empty<T>();
+
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                if (arr[i].GetType().Name == "Int32")
+                    
+                {
+                    if (Convert.ToInt32(arr[i]) == Convert.ToInt32(element))
+                    //if (arr[i].CompareTo(element) == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        newarr=newarr.Append(arr[i]).ToArray();
+                        Console.Write(arr[i] + ", ");
+                    }
+                }
+
+                else if (arr[i].GetType().Name == "Double")
+
+                {
+                    if (Convert.ToDouble (arr[i]) == Convert.ToDouble(element))
+                    //if (arr[i].CompareTo(element) == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        newarr = newarr.Append(arr[i]).ToArray();
+                        Console.Write(arr[i] + ", ");
+                    }
+                }
+                else if (arr[i].CompareTo(element) == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        newarr = newarr.Append(arr[i]).ToArray();
+                        Console.Write(arr[i] + ", ");
+                    }
+                }
+
+            Console.WriteLine("");
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                Console.Write(newarr[i] + ", ");
+            }
+            Console.WriteLine(element.GetType().Name);
+        }
+
+            
+    }
+
+
+}
