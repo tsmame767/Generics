7440dad569ffdbfb61f54741f395677a0882f721
tsmame767 <tsmane789@gmail.com>
2024-01-24 17:25:01 +0530

    prog4

---
+++ prog4/MinClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog4
+{
+
+
+    internal class MinClass<T> where T : IComparable<T>
+    {
+        /*
+        public Boolean CompareStr(string name1,string name2) {
+            int count_str1 = 0;
+            int count_str2 = 0;
+
+
+            return true;*/
+        
+
+        public int Minnum(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        public double MinDoub(Double[] arr)
+        {
+            Double min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+        public string MinStr(String[] arr)
+        {
+            String min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].CompareTo(min)>0)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+
+    }
+}
---
+++ prog4/main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog4
+{
+    internal class main
+    {
+
+        static void Main()
+        {
+
+            int[] arr1 = { 1,6, 2, 3, 4, 5 };
+            double[] arr2 = { 11.2, 6.77, 22.3, 33.4, 44.5, 55.6 };
+            string[] arr3 = { "Tushar", "Hemant", "Vikas" };
+
+            MinClass<int> obj = new MinClass<int>();
+            MinClass<double> obj2 = new MinClass<double>();
+            MinClass<string> obj3 = new MinClass<string>();
+
+            Console.WriteLine(obj.Minnum(arr1));
+            Console.WriteLine(obj.MinDoub(arr2));
+            Console.WriteLine(obj.MinStr(arr3));
+        }
+    }
+}
